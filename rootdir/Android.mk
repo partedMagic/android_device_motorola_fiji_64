4fed3170e883b65a7bad209158202a0fec2cc89b
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-05-08 17:20:56 +0100

    ef ef ar

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,13 +15,13 @@ LOCAL_SRC_FILES    := etc/init.recovery.mt6765.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.rc
+#LOCAL_MODULE_TAGS  := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.rc
+#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo lineage_fiji_64-user
