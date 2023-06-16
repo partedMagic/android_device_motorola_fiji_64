3c1c3dc85fbecb19dff1776a3214e0c5ce766fa7
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-06-16 21:30:13 +0300

    Update and rename lineage_fiji_64.mk to aosp_fiji_64.mk

--- aosp_fiji_64.mk
+++ aosp_fiji_64.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/android/config/common_full_phone.mk)
 
 # Inherit from fiji_64 device
 $(call inherit-product, device/motorola/fiji_64/device.mk)
 
 PRODUCT_DEVICE := fiji_64
-PRODUCT_NAME := lineage_fiji_64
+PRODUCT_NAME := aosp_fiji_64
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e6s
 PRODUCT_MANUFACTURER := motorola
