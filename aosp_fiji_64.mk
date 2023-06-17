26429e14720a2aa7c3fb2387df58091e0db134e0
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-06-17 12:01:47 +0300

    Update aosp_fiji_64.mk

--- aosp_fiji_64.mk
+++ aosp_fiji_64.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/android/config/common_full_phone.mk)
-
 # Inherit from fiji_64 device
 $(call inherit-product, device/motorola/fiji_64/device.mk)
 
