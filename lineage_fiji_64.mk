068f6138a9a97e2c9143a71e4b38f9b4b0f5ec97
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-06-24 19:14:35 +0300

    Update lineage_fiji_64.mk

--- lineage_fiji_64.mk
+++ lineage_fiji_64.mk
@@ -6,10 +6,11 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from fiji_64 device
 $(call inherit-product, device/motorola/fiji_64/device.mk)
